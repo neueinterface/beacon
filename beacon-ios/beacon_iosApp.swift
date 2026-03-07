be9656be78bd43d8b0bc8178da4eaa771673a8df
Armond Schneider <armondschneider@gmail.com>
2026-03-07 12:28:31 -0700

    chore: initial commit

---
+++ beacon-ios/beacon_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  beacon_iosApp.swift
+//  beacon-ios
+//
+//  Created by Armond Schneider on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct beacon_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
